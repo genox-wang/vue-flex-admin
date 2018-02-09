5a1d99bf2360522b3561f9b25703e28c1754de68
wangji <genius840215@163.com>
2018-02-09 12:35:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <boilerplate/>
+  </div>
+</template>
+
+<script>
+import Boilerplate from './components/Boilerplate';
+
+export default {
+  name: 'App',
+  data() {
+
+  },
+  components: {
+    Boilerplate,
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
